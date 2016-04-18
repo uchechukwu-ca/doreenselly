05038b58ff2e5d12676b789854cf2f4669b5f7a8
uchechukwu-ca <uchechukwu.ca@gmail.com>
2016-04-18 14:34:26 +0100

    Url Changed

--- doreenProject/urls.py
+++ doreenProject/urls.py
@@ -1,17 +1,17 @@
 """doreenProject URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/1.9/topics/http/urls/
+		https://docs.djangoproject.com/en/1.9/topics/http/urls/
 Examples:
 Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+		1. Add an import:  from my_app import views
+		2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
 Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+		1. Add an import:  from other_app.views import Home
+		2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
 Including another URLconf
-    1. Import the include() function: from django.conf.urls import url, include
-    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+		1. Import the include() function: from django.conf.urls import url, include
+		2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
@@ -21,13 +21,14 @@ from django.conf.urls.static import static
 
 urlpatterns = [
 	url(r'^admin/', admin.site.urls),
+	url(r'^$', 'doreenselly.views.index', name="index"),
 	url(r'^doreenselly/', include('doreenselly.urls')),
 	url(r'^delete_item/(?P<item_id>[-\w]+)/$', views.delete_item, name='delete_item'),
 ]
 
 if settings.DEBUG:
  urlpatterns += patterns(
-   'django.views.static',
-   (r'^media/(?P<path>.*)',
-   'serve',
-   {'document_root': settings.MEDIA_ROOT}), )
+	 'django.views.static',
+	 (r'^media/(?P<path>.*)',
+	 'serve',
+	 {'document_root': settings.MEDIA_ROOT}), )
